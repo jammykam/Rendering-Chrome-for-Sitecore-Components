de276f196423eb9cebc9da621c28e7e04a106df4
jammykam <jammykam@gmail.com>
2017-01-03 14:59:18 +0000

    Initial Commit for Sitecore 8.1.1+

---
+++ src/ForwardSlash.SC.RenderingChrome/HtmlHelpers/RenderingChromeExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Web;
+using Sitecore.Configuration;
+using Sitecore.Mvc.Helpers;
+
+namespace ForwardSlash.SC.RenderingChrome.HtmlHelpers
+{
+    public static class RenderingChromeExtensions
+    {
+        private const string DisplayExtendedInfo = "RenderingChrome.DisplayExtededInfo";
+
+        public static IHtmlString ContainerChrome(this SitecoreHelper helper)
+        {
+            return helper.ContainerChrome(null);
+        }
+
+        public static IHtmlString ContainerChrome(this SitecoreHelper helper, string title)
+        {
+            return helper.GetChromeAttribute(title, "container");
+        }
+
+        public static IHtmlString WidgetChrome(this SitecoreHelper helper)
+        {
+            return helper.WidgetChrome(null);
+        }
+
+        public static IHtmlString WidgetChrome(this SitecoreHelper helper, string title)
+        {
+            return helper.GetChromeAttribute(title, "widget");
+        }
+
+        private static IHtmlString GetChromeAttribute(this SitecoreHelper helper, string title, string attribute)
+        {
+            /* Earlier version of Sitecore? Use: Sitecore.Context.PageMode.IsPageEditorEditing */
+            if (!Sitecore.Context.PageMode.IsExperienceEditorEditing)
+                return null;
+
+            if (string.IsNullOrWhiteSpace(title))
+                title = helper.CurrentRendering.RenderingItem.DisplayName;
+
+            if (Settings.GetBoolSetting(DisplayExtendedInfo, false))
+                title += $" ({helper.CurrentRendering.Renderer})";
+
+            string htmlString = $"data-{attribute}-title=\"{title}\"" + WrapperTitle(title);
+            return new HtmlString(htmlString);
+        }
+
+        private static string WrapperTitle(string title)
+        {
+            return Settings.GetBoolSetting(DisplayExtendedInfo, false) ? $" title=\"{title}\"" : null;
+        }
+    }
+}
